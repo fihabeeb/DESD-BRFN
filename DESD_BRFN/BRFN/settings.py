24819f4ad6a83147ae5e9c6d23ae35f889f13b8b
saif <41209479+TEHEORY@users.noreply.github.com>
2026-02-27 13:00:43 +0000

    Fixing customers model

--- DESD_BRFN/BRFN/settings.py
+++ DESD_BRFN/BRFN/settings.py
@@ -37,11 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-<<<<<<< HEAD:DESD_BRFN/BRFN/settings.py
-    'products'
-=======
-    'customers'
->>>>>>> saif:BRFN/settings.py
+    'products',
+    'customers',
 ]
 
 MIDDLEWARE = [
