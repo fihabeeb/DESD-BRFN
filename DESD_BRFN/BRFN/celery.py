4e223574fc84e82194c98d108a44275651ca9d53
jakura4life <aliff2.azizulazily@live.uwe.ac.uk>
2026-04-13 11:27:17 +0100

    celery-scheduler: change from crontab to intervals for now

--- DESD_BRFN/BRFN/celery.py
+++ DESD_BRFN/BRFN/celery.py
@@ -2,6 +2,7 @@ import os
 
 from celery import Celery
 from celery.schedules import crontab
+from datetime import timedelta
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BRFN.settings')
@@ -26,7 +27,8 @@ def debug_task(self):
 app.conf.beat_schedule = {
     'weekly-payment-settlements': {
         'task': 'payments.tasks.process_weekly_settlements',
-        'schedule': crontab(day_of_week='monday', hour=0, minute=0),  # sunday midnight / Monday
+        # 'schedule': crontab(day_of_week='monday', hour=0, minute=0),  # sunday midnight / Monday
+        'schedule': timedelta(days=1),
         'options': {
             'expires': 86400,  # Task expires after 1 day if not started
         }
@@ -35,7 +37,8 @@ app.conf.beat_schedule = {
     # removes orders that are expired in the db (user left during checkout process)
     'delete-zombie-orders': {
         'task': 'orders.tasks.cleanup_expired_orders',
-        'schedule': crontab(minute=30),
+        # 'schedule': crontab(minute=30),
+        'schedule': timedelta(minutes=30),
         'options': {
             'expires': 1500,  # expires 25 mins before.
         }
